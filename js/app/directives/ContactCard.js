85090a503c4ab49939d8df113055df5425b87e4c
McN33ly <rmcneely23@gmail.com>
2016-07-26 11:10:09 -0400

    Done.

--- js/app/directives/ContactCard.js
+++ js/app/directives/ContactCard.js
@@ -1,22 +1,24 @@
 function ContactCard() {
 	return {
-		scope: {
+		scope: {},
+		controller: function(){},
+		controllerAs: 'contact',
+		bindToController:  {
 			name: '=',
 			email: '=',
 			phone: '=',
-			username: '='
-		},
+			username: '='},
 		template: [
 			'<div>',
 				'<h4>Contact Card</h4>',
 				'<label>Name:</label>',
-				'{{ name }}',
+				'{{ contact.name }}',
 				'<label>Email:</label>',
-				'{{ email }}',
+				'{{ contact.email }}',
 				'<label>Phone:</label>',
-				'{{ phone }}',
+				'{{ contact.phone }}',
 				'<label>Username:</label>',
-				'<span class="username">{{ username }}</span>',
+				'<span class="username">{{ contact.username }}</span>',
 			'</div>'
 		].join(''),
 		restrict: 'E'
